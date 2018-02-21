8182f44847f09ad6ed23b80e038ba8cd59fdca58
DESKTOP-3QER04D\can <yigitcanparilti@gmail.com>
2018-02-21 16:14:05 +0300

    Initial Commit.

---
+++ PdfMerger/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace PdfMerger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChoose = new System.Windows.Forms.Button();
+            this.btnMergeAndSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnChoose
+            // 
+            this.btnChoose.Location = new System.Drawing.Point(12, 12);
+            this.btnChoose.Name = "btnChoose";
+            this.btnChoose.Size = new System.Drawing.Size(131, 44);
+            this.btnChoose.TabIndex = 0;
+            this.btnChoose.Text = "Choose PDF Files";
+            this.btnChoose.UseVisualStyleBackColor = true;
+            this.btnChoose.Click += new System.EventHandler(this.btnChoose_Click);
+            // 
+            // btnMergeAndSave
+            // 
+            this.btnMergeAndSave.Location = new System.Drawing.Point(159, 12);
+            this.btnMergeAndSave.Name = "btnMergeAndSave";
+            this.btnMergeAndSave.Size = new System.Drawing.Size(131, 44);
+            this.btnMergeAndSave.TabIndex = 1;
+            this.btnMergeAndSave.Text = "Merge And Save";
+            this.btnMergeAndSave.UseVisualStyleBackColor = true;
+            this.btnMergeAndSave.Click += new System.EventHandler(this.btnMergeAndSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 68);
+            this.Controls.Add(this.btnMergeAndSave);
+            this.Controls.Add(this.btnChoose);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PDF Merger";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChoose;
+        private System.Windows.Forms.Button btnMergeAndSave;
+    }
+}
+
---
+++ PdfMerger/Form1.cs
@@ -0,0 +1,81 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PdfMerger
+{
+    public partial class Form1 : Form
+    {
+        private List<string> pdfFilePaths;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnMergeAndSave.Enabled = false;
+
+        }
+
+        private void btnChoose_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "PDF files(*.pdf)| *.pdf";
+            openFileDialog.Title = "Choose pdf files";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pdfFilePaths = new List<string>();
+                pdfFilePaths.AddRange(openFileDialog.FileNames);
+                btnMergeAndSave.Enabled = pdfFilePaths.Count > 1;
+            }
+        }
+
+        private void btnMergeAndSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PDF files(*.pdf)| *.pdf";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                MergeFilesInPathListThenSave(saveFileDialog.FileName);
+                MessageBox.Show("Merging files process has been completed.");
+                pdfFilePaths.Clear();
+                btnMergeAndSave.Enabled = pdfFilePaths.Count > 1;
+            }
+
+        }
+
+        private void MergeFilesInPathListThenSave(string path)
+        {
+            using (PdfDocument mergedPdf = new PdfDocument())
+            {
+                foreach (var filename in pdfFilePaths)
+                {
+                    using (PdfDocument pdf = PdfReader.Open(filename, PdfDocumentOpenMode.Import))
+                    {
+                        CopyPages(pdf, mergedPdf);
+                    }
+                }
+                mergedPdf.Save(path);
+            }
+        }
+
+        private void CopyPages(PdfDocument from, PdfDocument to)
+        {
+            for (int i = 0; i < from.PageCount; i++)
+            {
+                to.AddPage(from.Pages[i]);
+            }
+        }
+    }
+}
